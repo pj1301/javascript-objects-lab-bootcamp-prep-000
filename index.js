fc11db16c370587f720aa3222e6ce8fd6e66e493
James Hayward <james.hayward6@icloud.com>
2019-04-16 04:13:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 destructivelyUpdateObjectWithKeyAndValue(recipes, 'prop2', 2);
 
 function deleteFromObjectByKey(object, key) {
-  return Object.assign({}, object, { [key]: undefined })
+  return Object.assign({}, object, { [key]: undefined }) // rather than delete we make the value of the key undefined using the non-destructive additive statement. This will change the statement.
 }
 
 deleteFromObjectByKey(recipes, 'prop')
