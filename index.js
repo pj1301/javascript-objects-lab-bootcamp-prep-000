02c7d07e24245da4b96eee5bea3b4aaf15fd5806
James Hayward <james.hayward6@icloud.com>
2019-04-16 03:55:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ var recipes = {
 function updateObjectWithKeyAndValue(recipes, key, value) {
   return Object.assign({}, recipes, {key: value})
 }
+
+updateObjectWithKeyAndValue(recipes, 'prop2', 2);
